eb01a4e0f4e65e90661ac968e7d2676ae5b56110
Sabine Robart <sabine.robart@protonmail.com>
2019-08-01 16:13:44 +0200

    [Add] homepage : infos + design

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,9 +1,33 @@
 import React from "react";
 import "../style/Homepage.scss";
+import { Row } from "reactstrap";
 
 const Homepage = () => {
   const user = JSON.parse(localStorage.getItem("user"));
-  return <div className="homepage">Hello, {user.nickname}</div>;
+  console.log(user);
+  return (
+    <div className="homepage">
+      <Row className="title shadow-sm flex-column">
+        <h2>{"user" in localStorage ? `${user.fullName}` : "Prénom Nom"}</h2>
+        <div className="separation" />
+        <h1>
+          Titre professionnel
+          <br />
+          Développeur Web & Web mobile
+        </h1>
+        <div className="separation align-self-end" />
+        <h4>
+          Session{" "}
+          {"user" in localStorage
+            ? `${user.month} ${user.year}`
+            : ` _________ ____`}
+        </h4>
+      </Row>
+      <svg width="850" height="400" className="frame">
+        <rect width="850" height="350" />
+      </svg>
+    </div>
+  );
 };
 
 export default Homepage;
