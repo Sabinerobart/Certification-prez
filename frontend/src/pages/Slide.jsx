ec8fb7cd085f12d1421f8fa8829832c3e7830d7b
Sabine Robart <sabine.robart@protonmail.com>
2019-08-02 18:40:52 +0200

    [Add] connection to the redux store (+ condition to render a loading component while fetching data)

--- frontend/src/pages/Slide.jsx
+++ frontend/src/pages/Slide.jsx
@@ -3,8 +3,10 @@ import axios from "axios";
 import "../style/Slide.scss";
 import { Form, FormGroup, Input } from "reactstrap";
 import { backend } from "../conf.js";
+import { connect } from "react-redux";
+import Loader from "../components/Loader";
 
-export default class Slide extends Component {
+class Slide extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -14,8 +16,8 @@ export default class Slide extends Component {
       bullet_point_two: "2nd bullet point",
       bullet_point_three: "2rd bullet point",
       bullet_point_four: null,
-      profile: "stranger",
-      img: null
+      img: null,
+      loading: true
     };
   }
 
@@ -29,14 +31,10 @@ export default class Slide extends Component {
         bullet_point_two: data.bullet_point_two,
         bullet_point_three: data.bullet_point_three,
         bullet_point_four: data.bullet_point_four,
-        img: data.img
+        img: data.img,
+        loading: false
       });
     });
-
-    if ("user" in localStorage) {
-      const user = JSON.parse(localStorage.getItem("user"));
-      this.setState({ profile: user });
-    }
   }
 
   handleChange(event) {
@@ -67,108 +65,122 @@ export default class Slide extends Component {
   // }
 
   render() {
-    const user = this.state.profile;
+    const { user } = this.props;
     let disabled = true;
-    return (
-      <div className="slide-page center">
-        <img
-          src={this.state.img}
-          alt={`img-${this.props.match.params.id}`}
-          className="slide-img"
-        />
-        <div className="underlay" />
-        <Form
-          onSubmit={e => {
-            this.handleSubmit(e);
-          }}
-          className="d-flex flex-column"
-        >
-          <FormGroup>
-            <Input
-              type="text"
-              name="title"
-              id="title"
-              value={this.state.title}
-              onChange={e => {
-                this.handleChange(e);
-              }}
-              disabled={user.is_admin ? "" : disabled}
-            />
-          </FormGroup>
-          <div className="separation" />
-          <FormGroup>
-            <Input
-              type="text"
-              name="description"
-              id="description"
-              value={this.state.description}
-              onChange={e => {
-                this.handleChange(e);
-              }}
-              disabled={user.is_admin ? "" : disabled}
-            />
-          </FormGroup>
-          <FormGroup>
-            <Input
-              type="text"
-              name="bullet_point_one"
-              id="bullet_point_one"
-              value={this.state.bullet_point_one}
-              onChange={e => {
-                this.handleChange(e);
-              }}
-              disabled={user.is_admin ? "" : disabled}
-            />
-          </FormGroup>
-          <FormGroup>
-            <Input
-              type="text"
-              name="bullet_point_two"
-              id="bullet_point_two"
-              value={this.state.bullet_point_two}
-              onChange={e => {
-                this.handleChange(e);
-              }}
-              disabled={user.is_admin ? "" : disabled}
-            />
-          </FormGroup>
-          <FormGroup>
-            <Input
-              type="text"
-              name="bullet_point_three"
-              id="bullet_point_three"
-              value={this.state.bullet_point_three}
-              onChange={e => {
-                this.handleChange(e);
-              }}
-              disabled={user.is_admin ? "" : disabled}
-            />
-          </FormGroup>
-          {this.state.bullet_point_four !== null ? (
+    if (this.state.loading) {
+      return <Loader />;
+    } else {
+      return (
+        <div className="slide-page center">
+          <img
+            src={this.state.img}
+            alt={`img-${this.props.match.params.id}`}
+            className="slide-img"
+          />
+          <div className="underlay" />
+          <Form
+            onSubmit={e => {
+              this.handleSubmit(e);
+            }}
+            className="d-flex flex-column"
+          >
             <FormGroup>
               <Input
                 type="text"
-                name="bullet_point_four"
-                id="bullet_point_four"
-                value={this.state.bullet_point_four}
+                name="title"
+                id="title"
+                value={this.state.title}
                 onChange={e => {
                   this.handleChange(e);
                 }}
-                disabled={user.is_admin ? "" : disabled}
+                disabled={user && user.is_admin ? "" : disabled}
               />
             </FormGroup>
-          ) : null}
-          <button
-            style={{ border: "1px solid black" }}
-            block
-            className={user.is_admin ? "slide-form-btn" : "hide-btn"}
-            type="submit"
-            tabIndex="4"
-          >
-            Send
-          </button>
-        </Form>
-      </div>
-    );
+            <div className="separation" />
+            <FormGroup>
+              <Input
+                type="text"
+                name="description"
+                id="description"
+                value={this.state.description}
+                onChange={e => {
+                  this.handleChange(e);
+                }}
+                disabled={user && user.is_admin ? "" : disabled}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Input
+                type="text"
+                name="bullet_point_one"
+                id="bullet_point_one"
+                value={this.state.bullet_point_one}
+                onChange={e => {
+                  this.handleChange(e);
+                }}
+                disabled={user && user.is_admin ? "" : disabled}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Input
+                type="text"
+                name="bullet_point_two"
+                id="bullet_point_two"
+                value={this.state.bullet_point_two}
+                onChange={e => {
+                  this.handleChange(e);
+                }}
+                disabled={user && user.is_admin ? "" : disabled}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Input
+                type="text"
+                name="bullet_point_three"
+                id="bullet_point_three"
+                value={this.state.bullet_point_three}
+                onChange={e => {
+                  this.handleChange(e);
+                }}
+                disabled={user && user.is_admin ? "" : disabled}
+              />
+            </FormGroup>
+            {this.state.bullet_point_four !== null ? (
+              <FormGroup>
+                <Input
+                  type="text"
+                  name="bullet_point_four"
+                  id="bullet_point_four"
+                  value={this.state.bullet_point_four}
+                  onChange={e => {
+                    this.handleChange(e);
+                  }}
+                  disabled={user && user.is_admin ? "" : disabled}
+                />
+              </FormGroup>
+            ) : null}
+            <button
+              style={{ border: "1px solid black" }}
+              block
+              className={user && user.is_admin ? "slide-form-btn" : "hide-btn"}
+              type="submit"
+              tabIndex="4"
+            >
+              Send
+            </button>
+          </Form>
+        </div>
+      );
+    }
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    user: state.loginReducer.state
+  };
+};
+
+const SlideContainer = connect(mapStateToProps)(Slide);
+
+export default SlideContainer;
