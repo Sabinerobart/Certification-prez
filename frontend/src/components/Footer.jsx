b4db6c20762b3f5844aa5449475f431ba6eafa00
Sabine Robart <sabine.robart@protonmail.com>
2019-08-02 14:37:10 +0200

    [Refa] the footer into a class component to get the user's infos from the redux store

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import "../style/Footer.scss";
+import { connect } from "react-redux";
 
-const Footer = () => {
-  return (
-    <div className="footer d-flex pt-3 px-5">
-      Sabine Robart
-      <div className="line" />
-      Titre professionnel Développeur web & web mobile
-      <div className="line" />
-      2020
-    </div>
-  );
+class Footer extends React.Component {
+  render() {
+    const { user } = this.props;
+    const profile = user && user.state;
+    return (
+      <div className="footer d-flex pt-3 px-5">
+        {user ? `${profile.fullName}` : "Sabine Robart"}
+        <div className="line" />
+        Titre professionnel Développeur web & web mobile
+        <div className="line" />
+        {user ? `${profile.month} ${profile.year}` : `février 2019`}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    user: state.loginReducer
+  };
 };
 
-export default Footer;
+const FooterContainer = connect(mapStateToProps)(Footer);
+
+export default FooterContainer;
