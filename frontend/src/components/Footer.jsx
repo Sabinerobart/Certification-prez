940e38d82a5911c087d652e99686c23fbb2c18f0
Sabine Robart <sabine.robart@protonmail.com>
2019-08-02 18:31:11 +0200

    [Refa] back to a functional component since I managed to get the needed infos from the redux store

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,25 +2,24 @@ import React from "react";
 import "../style/Footer.scss";
 import { connect } from "react-redux";
 
-class Footer extends React.Component {
-  render() {
-    const { user } = this.props;
-    const profile = user && user.state;
-    return (
-      <div className="footer d-flex pt-3 px-5">
-        {user ? `${profile.fullName}` : "Sabine Robart"}
-        <div className="line" />
-        Titre professionnel Développeur web & web mobile
-        <div className="line" />
-        {user ? `${profile.month} ${profile.year}` : `février 2019`}
-      </div>
-    );
-  }
-}
+const Footer = user => {
+  const profile = user.user;
+  return (
+    <div className="footer d-flex pt-3 px-5">
+      {user ? `${profile && profile.fullName}` : "Sabine Robart"}
+      <div className="line" />
+      <em>Titre professionnel Développeur web & web mobile</em>
+      <div className="line" />
+      {user
+        ? `${profile && profile.month} ${profile && profile.year}`
+        : `février 2019`}
+    </div>
+  );
+};
 
 const mapStateToProps = state => {
   return {
-    user: state.loginReducer
+    user: state.loginReducer.state
   };
 };
 
