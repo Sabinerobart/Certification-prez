54771309059b4993785ec4c719938359007ec48a
Sabine Robart <sabine.robart@protonmail.com>
2019-08-04 19:11:01 +0200

    [Fix] backend routes (path coming from variable) + conditional rendering on several elements (images, bullet points...)

--- frontend/src/pages/Slide.jsx
+++ frontend/src/pages/Slide.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import "../style/Slide.scss";
-import { Form, FormGroup, Input } from "reactstrap";
+import { Row, Col, Form, FormGroup, Input } from "reactstrap";
 import { backend } from "../conf.js";
 import { connect } from "react-redux";
 import Loader from "../components/Loader";
@@ -15,15 +15,17 @@ class Slide extends Component {
       bullet_point_one: "1st bullet point",
       bullet_point_two: "2nd bullet point",
       bullet_point_three: "2rd bullet point",
-      bullet_point_four: null,
-      img: null,
+      bullet_point_four: "",
+      img: "",
+      page_title: "",
+      logo: "",
       loading: true
     };
   }
 
   componentDidMount() {
     const category = this.props.match.params.id;
-    axios.get(`${backend}${category}`).then(({ data }) => {
+    axios.get(`${backend}/${category}`).then(({ data }) => {
       this.setState({
         title: data.title,
         description: data.description,
@@ -32,6 +34,8 @@ class Slide extends Component {
         bullet_point_three: data.bullet_point_three,
         bullet_point_four: data.bullet_point_four,
         img: data.img,
+        page_title: data.page_title,
+        logo: data.logo,
         loading: false
       });
     });
@@ -47,36 +51,45 @@ class Slide extends Component {
     event.preventDefault();
     const category = this.props.match.params.id;
     axios
-      .put(`${backend}${category}`, {
+      .put(`${backend}/${category}`, {
         title: this.state.title,
         description: this.state.description,
         bullet_point_one: this.state.bullet_point_one,
         bullet_point_two: this.state.bullet_point_two,
         bullet_point_three: this.state.bullet_point_three,
-        bullet_point_four: this.state.bullet_point_four
+        bullet_point_four: this.state.bullet_point_four,
+        page_title: this.state.page_title
       })
       .catch(err => {
         console.log(`Nope! ${err}`);
       });
   }
 
-  // validateForm(){
-
+  // validateForm() {
+  //   return this.state.title.length > 0;
   // }
 
   render() {
     const { user } = this.props;
+    const profile = user && user.user;
     let disabled = true;
+    const img = this.state.img;
+    const logo = this.state.logo;
+    const slideTitleClassName = logo
+      ? "slide-title justify-content-between align-items-center"
+      : "slide-title justify-content-end align-items-center";
     if (this.state.loading) {
       return <Loader />;
     } else {
       return (
         <div className="slide-page center">
-          <img
-            src={this.state.img}
-            alt={`img-${this.props.match.params.id}`}
-            className="slide-img"
-          />
+          {img ? (
+            <img
+              src={img}
+              alt={`img-${this.props.match.params.id}`}
+              className="slide-img"
+            />
+          ) : null}
           <div className="underlay" />
           <Form
             onSubmit={e => {
@@ -84,91 +97,139 @@ class Slide extends Component {
             }}
             className="d-flex flex-column"
           >
-            <FormGroup>
-              <Input
-                type="text"
-                name="title"
-                id="title"
-                value={this.state.title}
-                onChange={e => {
-                  this.handleChange(e);
-                }}
-                disabled={user && user.is_admin ? "" : disabled}
-              />
-            </FormGroup>
-            <div className="separation" />
-            <FormGroup>
-              <Input
-                type="text"
-                name="description"
-                id="description"
-                value={this.state.description}
-                onChange={e => {
-                  this.handleChange(e);
-                }}
-                disabled={user && user.is_admin ? "" : disabled}
-              />
-            </FormGroup>
-            <FormGroup>
-              <Input
-                type="text"
-                name="bullet_point_one"
-                id="bullet_point_one"
-                value={this.state.bullet_point_one}
-                onChange={e => {
-                  this.handleChange(e);
-                }}
-                disabled={user && user.is_admin ? "" : disabled}
-              />
-            </FormGroup>
-            <FormGroup>
-              <Input
-                type="text"
-                name="bullet_point_two"
-                id="bullet_point_two"
-                value={this.state.bullet_point_two}
-                onChange={e => {
-                  this.handleChange(e);
-                }}
-                disabled={user && user.is_admin ? "" : disabled}
-              />
-            </FormGroup>
-            <FormGroup>
-              <Input
-                type="text"
-                name="bullet_point_three"
-                id="bullet_point_three"
-                value={this.state.bullet_point_three}
-                onChange={e => {
-                  this.handleChange(e);
-                }}
-                disabled={user && user.is_admin ? "" : disabled}
-              />
-            </FormGroup>
-            {this.state.bullet_point_four !== null ? (
+            <Row className={slideTitleClassName}>
+              {logo ? (
+                <img src={logo} alt={this.state.page_title + "-logo"} />
+              ) : null}
               <FormGroup>
                 <Input
                   type="text"
-                  name="bullet_point_four"
-                  id="bullet_point_four"
-                  value={this.state.bullet_point_four}
+                  name="page_title"
+                  id="page_title"
+                  value={this.state.page_title}
                   onChange={e => {
                     this.handleChange(e);
                   }}
-                  disabled={user && user.is_admin ? "" : disabled}
+                  disabled={
+                    user ? (profile.is_admin ? "" : disabled) : disabled
+                  }
+                  className="page_title"
                 />
               </FormGroup>
-            ) : null}
+            </Row>
+            <Col xs="10" className="offset-1">
+              <FormGroup>
+                <Input
+                  type="text"
+                  name="title"
+                  id="title"
+                  value={this.state.title}
+                  onChange={e => {
+                    this.handleChange(e);
+                  }}
+                  disabled={
+                    user ? (profile.is_admin ? "" : disabled) : disabled
+                  }
+                />
+              </FormGroup>
+              <div className="separation" />
+              <FormGroup>
+                <Input
+                  type="text"
+                  name="description"
+                  id="description"
+                  value={this.state.description}
+                  onChange={e => {
+                    this.handleChange(e);
+                  }}
+                  disabled={
+                    user ? (profile.is_admin ? "" : disabled) : disabled
+                  }
+                />
+              </FormGroup>
+              <Row>
+                <Col md="6" xl="3" className="bullet-point">
+                  <FormGroup className="bg-light">
+                    <Input
+                      type="textarea"
+                      name="bullet_point_one"
+                      id="bullet_point_one"
+                      value={this.state.bullet_point_one}
+                      onChange={e => {
+                        this.handleChange(e);
+                      }}
+                      disabled={
+                        user ? (profile.is_admin ? "" : disabled) : disabled
+                      }
+                    />
+                  </FormGroup>
+                </Col>
+                <Col md="6" xl="3" className="bullet-point">
+                  <FormGroup className="bg-light">
+                    <Input
+                      type="textarea"
+                      name="bullet_point_two"
+                      id="bullet_point_two"
+                      value={this.state.bullet_point_two}
+                      onChange={e => {
+                        this.handleChange(e);
+                      }}
+                      disabled={
+                        user ? (profile.is_admin ? "" : disabled) : disabled
+                      }
+                    />
+                  </FormGroup>
+                </Col>
+                {this.state.bullet_point_three ? (
+                  <Col md="6" xl="3" className="bullet-point">
+                    <FormGroup className="bg-light">
+                      <Input
+                        type="textarea"
+                        name="bullet_point_three"
+                        id="bullet_point_three"
+                        value={this.state.bullet_point_three}
+                        onChange={e => {
+                          this.handleChange(e);
+                        }}
+                        disabled={
+                          user ? (profile.is_admin ? "" : disabled) : disabled
+                        }
+                      />
+                    </FormGroup>
+                  </Col>
+                ) : null}
+                {this.state.bullet_point_four ? (
+                  <Col md="6" xl="3" className="bullet-point">
+                    <FormGroup className="bg-light">
+                      <Input
+                        type="textarea"
+                        name="bullet_point_four"
+                        id="bullet_point_four"
+                        value={this.state.bullet_point_four}
+                        onChange={e => {
+                          this.handleChange(e);
+                        }}
+                        disabled={
+                          user ? (profile.is_admin ? "" : disabled) : disabled
+                        }
+                      />
+                    </FormGroup>
+                  </Col>
+                ) : null}
+              </Row>
+            </Col>
             <button
               style={{ border: "1px solid black" }}
               block
               className={user && user.is_admin ? "slide-form-btn" : "hide-btn"}
               type="submit"
               tabIndex="4"
+              // disabled={!this.validateForm()}
             >
               Send
             </button>
           </Form>
+          <div className="across" />
         </div>
       );
     }
