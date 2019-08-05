a582adae53a30bc7ca372e9e0d196df7146c1ecd
Sabine Robart <sabine.robart@protonmail.com>
2019-08-05 17:26:13 +0200

    [Add] the page's description part + ability to update the form (redux + onChange function)

--- frontend/src/pages/IntroConcl.jsx
+++ frontend/src/pages/IntroConcl.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import "../style/IntroConcl.scss";
-import { Row, Col, Input } from "reactstrap";
+import { Col, Form, FormGroup, Input } from "reactstrap";
 import axios from "axios";
 import { backend } from "../conf";
+import { connect } from "react-redux";
 
-export default class IntroConcl extends Component {
+class IntroConcl extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -12,7 +13,7 @@ export default class IntroConcl extends Component {
       description: "",
       bullet_point_one: "1st bullet point",
       bullet_point_two: "2nd bullet point",
-      bullet_point_three: "2rd bullet point",
+      bullet_point_three: "3rd bullet point",
       bullet_point_four: "",
       img: "",
       page_title: "",
@@ -64,33 +65,78 @@ export default class IntroConcl extends Component {
   }
 
   render() {
+    const user = this.props.user;
+    const profile = user && user.user;
+    let disabled = true;
     return (
-      <Row className="intro-concl_page center">
+      <Form
+        onSubmit={e => {
+          this.handleSubmit(e);
+        }}
+        className="d-flex intro-concl_page p-0 m-0"
+      >
         <Col md="6" className="intro-concl_title">
           <Input
             type="textarea"
-            name="page_title"
-            id="page_title"
+            name="title"
+            id="title"
             value={this.state.title}
-            // onChange={e => {
-            //   this.handleChange(e);
-            // }}
-            // disabled={user ? (profile.is_admin ? "" : disabled) : disabled}
+            onChange={e => {
+              this.handleChange(e);
+            }}
+            className="intro-title"
+            disabled={user ? (profile.is_admin ? "" : disabled) : disabled}
           />
         </Col>
-        <Col md="6" className="intro-concl_title">
+        <Col md="6" className="intro-concl_description center bg-dark px-5" />
+        <FormGroup>
+          <p
+            style={{
+              fontFamily: "RalewayBold",
+              textTransform: "uppercase",
+              fontSize: "2rem",
+              textAlign: "center",
+              padding: "3vh",
+              background: "#f5f5f5"
+            }}
+          >
+            {this.state.page_title}
+          </p>
           <Input
             type="textarea"
-            name="page_title"
-            id="page_title"
-            value={this.state.page_title}
-            // onChange={e => {
-            //   this.handleChange(e);
-            // }}
-            // disabled={user ? (profile.is_admin ? "" : disabled) : disabled}
+            name="description"
+            id="description"
+            value={this.state.description}
+            onChange={e => {
+              this.handleChange(e);
+            }}
+            disabled={user ? (profile.is_admin ? "" : disabled) : disabled}
           />
-        </Col>
-      </Row>
+        </FormGroup>
+        <button
+          className={
+            user
+              ? profile.is_admin
+                ? "slide-form-btn"
+                : "hide-btn"
+              : "hide-btn"
+          }
+          type="submit"
+          // disabled={!this.validateForm()}
+        >
+          Send
+        </button>
+      </Form>
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    user: state.loginReducer.state
+  };
+};
+
+const IntroConclContainer = connect(mapStateToProps)(IntroConcl);
+
+export default IntroConclContainer;
